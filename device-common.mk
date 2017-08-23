66144103f61f6e41179ac932fd7f3c08b646c3fa
Luca Stefani <luca020400@lineageos.org>
2017-08-24 01:43:10 +0800

    flox: Build DRM HIDL HAL interface
    Change-Id: I10d5a1cd3a09be132f56fe59a2667236df3cb594

--- device-common.mk
+++ device-common.mk
@@ -82,7 +82,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
     
 # VNDK
 PRODUCT_COPY_FILES += \
