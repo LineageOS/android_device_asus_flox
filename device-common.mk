544c5891f4ed68dc0300d30c30843ade2465bc23
Paul Keith <javelinanddart@gmail.com>
2019-06-09 16:39:09 +0200

    flox: Build livedisplay sysfs service
     * Add supported interfaces to manifest while we're at it
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- device-common.mk
+++ device-common.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
     lights.msm8960
+    
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs    
 
 # Trust
 PRODUCT_PACKAGES += \
