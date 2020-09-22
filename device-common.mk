9b2beb07eefd23229781c6eac4b9c0dedeb2521a
LuK1337 <priv.luk@gmail.com>
2020-09-22 12:02:43 +0100

    flox: Set PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...
    
    Change-Id: I6d8492dd7c56bfdb2ca00f3477634a46fa55b1c0

--- device-common.mk
+++ device-common.mk
@@ -25,6 +25,9 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # A list of dpis to select prebuilt apk, in precedence order.
 PRODUCT_AAPT_PREBUILT_DPI := hdpi
 
+# HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
