c327d097a863e5942adfa42cdc67cf43d3d5b545
surblazer <matvej2002@gmail.com>
2020-11-29 01:19:55 +0300

    flox: binderize DRM HAL
    Change-Id: I4ee61999511460b34de24a9e0ac19e7273e5ecc5

--- device-common.mk
+++ device-common.mk
@@ -82,7 +82,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.3-service-lazy.clearkey
     
 # VNDK
 PRODUCT_COPY_FILES += \
