06e6ab7b74e44d8474fef64337ca5070c0e3d167
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    flox: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,64 +1,36 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 set -e
 
+DEVICE=flox
 VENDOR=asus
-DEVICE_COMMON=flox
-DEVICE=${DEVICE:-flox}
-export INITIAL_COPYRIGHT_YEAR=2013
 
-# Load extractutils and do some sanity checks
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
-# Copyright headers and guards
-write_headers "flox debx"
+# Warning headers and guards
+write_headers
 
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-blobs.txt
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-# Done
+# Finish
 write_footers
-
-if [ "$DEVICE" '!=' "$DEVICE_COMMON" ]; then
-	# Reinitialize the helper for the device-specific blobs
-	setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-	# Copyright headers and guards
-	write_headers
-
-	# The device-specific blobs
-	write_makefiles "$MY_DIR"/../$DEVICE/proprietary-blobs.txt
-
-	# Done
-	write_footers
-fi
-
-
