b447e455cf31246de0aec38c17c493a01bfb0fd6
Matsvei Niaverau <matvej2002@gmail.com>
2021-01-17 19:19:39 +0100

    Revert "flox: binderize DRM HAL"
    This reverts commit c327d097a863e5942adfa42cdc67cf43d3d5b545.
    
    Reason for revert: We don't really need this lazy HAL implementation, it's better to use the regular one. Also better to go inline with the 17.1 branch
    
    Change-Id: I6079d44faa23c0f137f133a5c538d256e6498f54

--- device-common.mk
+++ device-common.mk
@@ -82,10 +82,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service-lazy.clearkey
-    
+    android.hardware.drm@1.0-impl
+
 # VNDK
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so    
